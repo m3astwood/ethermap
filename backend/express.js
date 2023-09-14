9b8d7239b823572938fa1c862788c93744c2fd24
suroh <mx@suroh.tk>
2023-09-14 19:04:56 +0200

    added basic session and store setup

--- backend/express.js
+++ backend/express.js
@@ -1,18 +1,31 @@
- // web server
+// web server
 import express from 'express'
+import session from 'express-session'
 import cors from 'cors'
+
+import KnexSessionStore from 'connect-session-knex'
+const KSS = KnexSessionStore(session)
 
 // database
 import DB from './db/DB.js'
-import { Model } from 'objection'
 
-// database setup
-Model.knex(DB)
+// session store
+const store = new KSS({ knex: DB })
 
 // webserver setup
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.use(session({
+  secret: 'supersessionstoresecretsauce',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
+  store
+}))
 
 // routes
 import apiRouter from './routes/api.js'
