04f98d4dc821d2a3190961d1d1eebb317e4322ce
suroh <mx@suroh.tk>
2023-10-17 00:45:11 +0200

    added event api controller for custom component events

---
+++ frontend/src/api/event.js
@@ -0,0 +1,28 @@
+export default class EventController {
+  constructor(host) {
+    this.host = host
+    this.host.addController(this)
+  }
+
+  hostConnected() {
+    console.log('EventController connected')
+  }
+
+  hostDisconnected() {
+    console.log('EventController disconnected')
+  }
+
+  dispatch(event, options) {
+    options = options || {}
+
+    const opts = {
+      bubbles: true,
+      composed: true,
+      ...options
+    }
+
+    const e = new CustomEvent(event, opts)
+
+    this.host.dispatchEvent(e)
+  }
+}
