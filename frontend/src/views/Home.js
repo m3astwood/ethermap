0e714f289764584230e1e852c7ccad16648a90bb
suroh <mx@suroh.tk>
2023-10-29 19:15:31 +0100

    added home view

---
+++ frontend/src/views/Home.js
@@ -0,0 +1,28 @@
+import { LitElement, css, html } from "lit";
+
+import '../components/NewMapModal.js'
+import '../components/LeafletMap.js'
+
+class HomeView extends LitElement {
+  static properties = {}
+
+  constructor() {
+    super()
+  }
+
+  render() {
+    return html`
+      <em-leaflet-map></em-leaflet-map>
+      <newmap-modal></newmap-modal>
+    `
+  }
+
+  static styles = css`
+    :host {
+      display: flex;
+      flex-grow: 1;
+    }
+  `
+}
+
+customElements.define('home-view', HomeView)
