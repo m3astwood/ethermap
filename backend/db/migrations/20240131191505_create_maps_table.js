5b6519bf28158c03e3e4b632bf773c473bea6bdc
suroh <mx@suroh.tk>
2024-01-31 21:03:06 +0100

    refactored migrations using knex cli

---
+++ backend/db/migrations/20240131191505_create_maps_table.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema
+    .createTable('maps', (table) => {
+      table.increments().primary()
+      table.string('name').notNullable().unique()
+      table.timestamps(false, true)
+    })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  if (knex.client.version === 'pg' || knex.client.version === 'pg-mem') {
+    return knex.schema
+      .raw('DROP TABLE maps CASCADE')
+  } else {
+    return knex.schema
+      .dropTable('maps')
+  }
+}
--- backend/db/migrations/20240131191609_create_points_table.js
+++ backend/db/migrations/20240131191609_create_points_table.js
@@ -2,21 +2,14 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const up = (knex) => {
+export function up(knex) {
   return knex.schema
-    .createTable('maps', (table) => {
-      table.increments().primary()
-      table.string('name').notNullable().unique()
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-      table.timestamp('updated_at').defaultTo(knex.fn.now())
-    })
     .createTable('map_points', (table) => {
       table.increments().primary()
       table.string('name')
       table.string('notes')
       table.point('location')
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-      table.timestamp('updated_at').defaultTo(knex.fn.now())
+      table.timestamps(false, true)
       table.integer('map_id').references('id').inTable('maps').onDelete('CASCADE')
     })
 }
@@ -25,16 +18,12 @@ const up = (knex) => {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const down = (knex) => {
+export function down(knex) {
   if (knex.client.version === 'pg' || knex.client.version === 'pg-mem') {
     return knex.schema
-      .raw('DROP TABLE maps CASCADE')
       .dropTable('map_points')
   } else {
     return knex.schema
-      .dropTable('maps')
       .dropTable('map_points')
   }
 }
-
-export { up, down }
