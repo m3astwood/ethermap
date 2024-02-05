49ac2f4e62d71e1b57decda918ad1c18ba01ed4f
suroh <mx@suroh.tk>
2024-02-05 19:21:41 +0100

    moved some websocket logic to REST paths

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-import App from './backend/express.js'
+import { app, server } from './backend/express.js'
 import ViteExpress from 'vite-express'
 import 'dotenv/config'
 
-import Session from './backend/middleware/sessions.js'
-import { Socket } from './backend/sockets.js'
-import { Server } from 'socket.io'
+// import Session from './backend/middleware/sessions.js'
+// import { Socket } from './backend/sockets.js'
+// import { Server } from 'socket.io'
 
 
-const server = App.listen(process.env.PORT, () => {
-  console.log(`Ethermap listening for connections on port ${process.env.PORT}`)
-})
-
-const io = new Server(server)
-ViteExpress.bind(App, server)
-Socket(io, Session)
+ViteExpress.bind(app, server)
