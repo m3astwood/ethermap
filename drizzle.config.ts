70af101059c98795a9c563f69a1fc564add3b223
suroh <mx@suroh.tk>
2025-07-21 19:17:59 +0200

    Fix database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: './backend/db/schema',
   out: './backend/db/migrations',
   dbCredentials: {
-    url: `postgresql://${env.DB_USER}:${env.DB_PASS}@${env.DB_HOST}:${env.DB_PORT}`
+    url: `postgresql://${env.DB_USER}:${env.DB_PASS}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`
   }
 } satisfies Config
 
