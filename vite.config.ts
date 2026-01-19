3ca5b0f632b409886c1ca76e308b3d0f7d78f269
Max Franklin <max.franklin@arturia.com>
2026-01-19 13:41:01 +0100

    Revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,30 @@
 /// <reference types="vitest" />
 import path from 'node:path'
 import devServer from '@hono/vite-dev-server'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    root: './frontend/',
-    base: '/',
-    envDir: '../',
-    build: {
-      sourcemap: true,
-      target: ['esnext', 'edge100', 'firefox100', 'chrome100', 'safari18'],
-      outDir: '../dist/frontend',
+export default defineConfig({
+  root: './frontend/',
+  base: '/',
+  envDir: '../',
+  build: {
+    sourcemap: true,
+    target: ['esnext', 'edge100', 'firefox100', 'chrome100', 'safari18'],
+    outDir: '../dist/frontend',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('./'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve('./'),
-      },
-    },
-    test: {
-      root: './',
-      hookTimeout: 30000,
-    },
-    plugins: [
-      devServer({
-        export: 'app',
-        entry: 'backend/app.ts',
-        env
-      }),
-    ],
-  }
+  },
+  test: {
+    root: './',
+    hookTimeout: 30000,
+  },
+  plugins: [
+    devServer({
+      export: 'app',
+      entry: 'backend/app.ts',
+    }),
+  ],
 })
